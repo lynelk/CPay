7c1ca226bf4c401cba907bf1ef44a6a7ab663b8c
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-03 17:37:54 +0300

    Add reconciliation status constants

---
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/reconciliation/ReconStatus.java
@@ -0,0 +1,10 @@
+package net.citotech.cito.reconciliation;
+
+public final class ReconStatus {
+    public static final String UNMATCHED = "UNMATCHED";
+    public static final String MATCHED = "MATCHED";
+    public static final String MANUAL_MATCH = "MANUAL_MATCH";
+
+    private ReconStatus() {
+    }
+}
