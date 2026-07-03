7a36eea7901b20ea37116a8b758cec582f8aad91
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-03 11:14:22 +0300

    Use canonical legacy id for Safaricom adapter

--- InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/SafaricomMpesaAdapter.java
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/SafaricomMpesaAdapter.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SafaricomMpesaAdapter extends LegacyGatewayAdapter {
     public static final String CHANNEL_CODE = "safaricom_mpesa";
-    public static final String LEGACY_GATEWAY_ID = "SAFARICOM-MPESA";
 
     public SafaricomMpesaAdapter() {
-        super(CHANNEL_CODE, "Safaricom M-Pesa", "KE", "KES", LEGACY_GATEWAY_ID);
+        super(CHANNEL_CODE, "Safaricom M-Pesa", "KE", "KES", LegacyGatewayIds.SAFARICOM_MPESA, "25470", "25471", "25472", "25474", "25479", "25411");
     }
 }
