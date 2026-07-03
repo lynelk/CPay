b62fdaf0eb49cf0e701099a1d2d127acacbb19e5
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-03 11:08:28 +0300

    Centralize legacy gateway identifiers

---
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/LegacyGatewayIds.java
@@ -0,0 +1,12 @@
+package net.citotech.cito.gateway;
+
+/** Legacy gateway identifiers used by the existing transaction engine. */
+public final class LegacyGatewayIds {
+    private LegacyGatewayIds() {
+    }
+
+    public static final String MTN_MOMO = "mtnmomo";
+    public static final String AIRTEL_MONEY = "airtelmoney";
+    public static final String AIRTEL_OPEN_API = "airtelmoneyopenapi";
+    public static final String SAFARICOM_MPESA = "safaricom";
+}
