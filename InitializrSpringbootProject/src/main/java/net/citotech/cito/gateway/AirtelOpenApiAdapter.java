d25c1e013864e09ae7b5737e659a74f7dad324d3
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-03 11:14:12 +0300

    Use canonical legacy id for Airtel OpenAPI adapter

--- InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/AirtelOpenApiAdapter.java
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/AirtelOpenApiAdapter.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class AirtelOpenApiAdapter extends LegacyGatewayAdapter {
     public static final String CHANNEL_CODE = "airtel_open_api";
-    public static final String LEGACY_GATEWAY_ID = "AIRTEL-OPENAPI";
 
     public AirtelOpenApiAdapter() {
-        super(CHANNEL_CODE, "Airtel OpenAPI", "UG", "UGX", LEGACY_GATEWAY_ID);
+        super(CHANNEL_CODE, "Airtel OpenAPI", "UG", "UGX", LegacyGatewayIds.AIRTEL_OPEN_API, "25675", "25670", "25676");
     }
 }
