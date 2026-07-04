e652cef405c80a27a1940b7a3cd6897c08c898c0
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-04 14:00:35 +0300

    Use endpoint execution for MTN adapter

--- InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/MtnMomoAdapter.java
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/MtnMomoAdapter.java
@@ -1,5 +1,6 @@
 package net.citotech.cito.gateway;
 
+import net.citotech.cito.Model.GateWayResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,5 +9,15 @@ public class MtnMomoAdapter extends LegacyGatewayAdapter {
 
     public MtnMomoAdapter() {
         super(CHANNEL_CODE, "MTN MoMo", "UG", "UGX", LegacyGatewayIds.MTN_MOMO, "25677", "25678", "25676");
+    }
+
+    @Override
+    public GateWayResponse collect(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "MTN MoMo", "COLLECT", request);
+    }
+
+    @Override
+    public GateWayResponse payout(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "MTN MoMo", "PAYOUT", request);
     }
 }
