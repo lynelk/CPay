525f9fa95930c7b99e03f48e0e94bcbca693ccf3
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-04 14:01:20 +0300

    Use endpoint execution for Safaricom channel

--- InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/SafaricomMpesaAdapter.java
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/SafaricomMpesaAdapter.java
@@ -1,5 +1,6 @@
 package net.citotech.cito.gateway;
 
+import net.citotech.cito.Model.GateWayResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,5 +9,15 @@ public class SafaricomMpesaAdapter extends LegacyGatewayAdapter {
 
     public SafaricomMpesaAdapter() {
         super(CHANNEL_CODE, "Safaricom M-Pesa", "KE", "KES", LegacyGatewayIds.SAFARICOM_MPESA, "25470", "25471", "25472", "25474", "25479", "25411");
+    }
+
+    @Override
+    public GateWayResponse collect(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "Safaricom M-Pesa", "COLLECT", request);
+    }
+
+    @Override
+    public GateWayResponse payout(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "Safaricom M-Pesa", "PAYOUT", request);
     }
 }
