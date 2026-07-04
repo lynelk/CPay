8684077a34a7d14308afad44bf8a17becc449a6d
lynelk <86437305+lynelk@users.noreply.github.com>
2026-07-04 14:00:56 +0300

    Use endpoint execution for Airtel channel

--- InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/AirtelMoneyAdapter.java
+++ InitializrSpringbootProject/src/main/java/net/citotech/cito/gateway/AirtelMoneyAdapter.java
@@ -1,5 +1,6 @@
 package net.citotech.cito.gateway;
 
+import net.citotech.cito.Model.GateWayResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +8,16 @@ public class AirtelMoneyAdapter extends LegacyGatewayAdapter {
     public static final String CHANNEL_CODE = "airtel_money";
 
     public AirtelMoneyAdapter() {
-        super(CHANNEL_CODE, "Airtel Money", "UG", "UGX", LegacyGatewayIds.AIRTEL_MONEY, "25675", "25670", "25676");
+        super(CHANNEL_CODE, "Airtel", "UG", "UGX", LegacyGatewayIds.AIRTEL_MONEY, "25675", "25670", "25676");
+    }
+
+    @Override
+    public GateWayResponse collect(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "Airtel", "COLLECT", request);
+    }
+
+    @Override
+    public GateWayResponse payout(PaymentGatewayRequest request) {
+        return ProviderEndpointClient.execute(CHANNEL_CODE, "Airtel", "PAYOUT", request);
     }
 }
