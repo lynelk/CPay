b5226faccc5e438e694cafa2e0a4d70f3cdeef5a
lynelk <86437305+lynelk@users.noreply.github.com>
2026-08-09 22:22:34 +0300

    Protect merchant vending self-service with portal session filter

--- InitializrSpringbootProjectFresh/src/main/java/net/citotech/cito/config/LegacySessionAuthorizationFilter.java
+++ InitializrSpringbootProjectFresh/src/main/java/net/citotech/cito/config/LegacySessionAuthorizationFilter.java
@@ -25,6 +25,7 @@ public class LegacySessionAuthorizationFilter extends OncePerRequestFilter {
         "/api/v2/merchant-self-service/channels",
         "/api/v2/merchant-self-service/batches",
         "/api/v2/merchant-self-service/webhooks",
+        "/api/v2/merchant-self-service/vending",
         "/api/v2/portal"
     );
     private static final List<String> PUBLIC_SETTINGS_PATHS = List.of(
