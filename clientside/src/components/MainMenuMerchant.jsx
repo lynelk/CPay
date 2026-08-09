8dd659dc4f2d88e9458ad923b921a3d744bf31e3
lynelk <86437305+lynelk@users.noreply.github.com>
2026-08-09 22:24:48 +0300

    Add vending to merchant operations navigation

--- clientside/src/components/MainMenuMerchant.jsx
+++ clientside/src/components/MainMenuMerchant.jsx
@@ -14,6 +14,7 @@ const navGroups = [
     title: 'Operations',
     items: [
       { value: 'payments', text: 'Payments', Icon: Icons.PaymentsIcon },
+      { value: 'vending', text: 'Vending', Icon: Icons.StoreIcon },
       { value: 'webhooks', text: 'Webhooks', Icon: Icons.LightningIcon },
       { value: 'sms', text: 'SMS', Icon: Icons.SmsIcon },
       { value: 'transactions', text: 'Transactions', Icon: Icons.ReceiptIcon },
